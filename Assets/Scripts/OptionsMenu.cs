f2f63355dfcb22c9012a11e7edd1b88e51478902
Ethan Campbell <ecampbell@mcneese.edu>
2025-01-28 10:31:26 -0800

    Added toggle for Full Screen. Added dropdown for game resolution. Added dropdown for game quality

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,14 +1,58 @@
 // Contributors: Ethan Campbell
 // Created On 1/25/2025
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolution = 0;
+
+        for (int i = 0; i < resolutions.Length; i++) 
+        { 
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height) 
+            {
+                currentResolution = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolution;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex) 
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
     public void SetVolume(float volume) 
     {
         audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 }
