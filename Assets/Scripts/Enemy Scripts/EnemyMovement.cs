3bd849ed18cf1b44fe2fcaefca8a0704d0739974
Ethan Campbell <ecampbell@mcneese.edu>
2025-03-04 20:31:57 -0600

    Fixed issue with enemies walking through walls/buildings. Updated enemy prefabs

--- Assets/Scripts/Enemy Scripts/EnemyMovement.cs
+++ Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -8,6 +8,18 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    [SerializeField]
+    private float rotationSpeed;
+
+    [SerializeField]
+    private float obstacleCheckRadius;
+
+    [SerializeField]
+    private float obstacleCheckDistance;
+
+    [SerializeField]
+    private LayerMask obstacleLayerMask;
+
     private Rigidbody2D rigidbody;
     private PlayerAwarenessController controller;
     private Vector2 targetDirection;
@@ -16,12 +28,16 @@ public class EnemyMovement : MonoBehaviour
     private bool isKnockedBack = false;
     private float knockbackTimer = 0f;
     private float knockbackDuration = 0.2f;
+    private RaycastHit2D[] obstacleCollisions;
+    private float cooldown;
+    private Vector2 obstacleTargetDirection;
 
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         controller = GetComponent<PlayerAwarenessController>();
         animator = GetComponent<Animator>();
+        obstacleCollisions = new RaycastHit2D[10];
     }
 
     void FixedUpdate()
@@ -54,11 +70,26 @@ public class EnemyMovement : MonoBehaviour
     {
         if (controller.playerInRange)
         {
+            HandleRandomDirectionChange();
             targetDirection = controller.directionToPlayer;
+            HandleObstacles();
         }
         else
         {
             targetDirection = Vector2.zero;
+        }
+    }
+
+    private void HandleRandomDirectionChange() 
+    {
+        cooldown -= Time.deltaTime;
+        
+        if(cooldown <= 0) 
+        {
+            float angleChange = Random.Range(-90f, 90f);
+            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
+            targetDirection = rotation * targetDirection;
+            cooldown = Random.Range(1f, 5f);
         }
     }
 
@@ -67,6 +98,41 @@ public class EnemyMovement : MonoBehaviour
         animator.SetFloat("InputX", targetDirection.x);
         animator.SetFloat("InputY", targetDirection.y);
         animator.SetBool("isMoving", targetDirection != Vector2.zero);
+    }
+
+    private void HandleObstacles() 
+    {
+        cooldown -= Time.deltaTime;
+        var contactFilter = new ContactFilter2D();
+        contactFilter.SetLayerMask(obstacleLayerMask);
+
+        int numberOfCollisions = Physics2D.CircleCast(
+            transform.position,
+            obstacleCheckRadius,
+            transform.up,
+            contactFilter,
+            obstacleCollisions,
+            obstacleCheckDistance);
+
+        for (int i = 0; i < numberOfCollisions; i++) 
+        {
+            var obstacleCollision = obstacleCollisions[i];
+
+            if (obstacleCollision.collider.gameObject == gameObject)
+                continue;
+
+            if (cooldown <= 0) 
+            {
+                obstacleTargetDirection = obstacleCollision.normal;
+                cooldown = 0.5f;
+            }
+
+            var targetRotation = Quaternion.LookRotation(transform.forward, obstacleTargetDirection);
+            var rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            targetDirection = rotation * Vector2.up;
+            break;
+        }
     }
 
     private void SetVelocity()
