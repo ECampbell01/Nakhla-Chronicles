8b6017049928154ac06d0fb498bafd76362e55b0
Ethan Campbell <ecampbell@mcneese.edu>
2025-04-01 15:18:22 -0500

    Implement drag and drop for the inventory system

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,17 @@
+// Contributions: Ethan Campbell
+// Date Created: 4/1/2025
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount == 0) 
+        {
+            InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
+            inventoryItem.parentAfterDrag = transform;
+        }
+    }
+}
