7cf08c14d17c7b9f3dcbe1373feed0ae9a8e9329
msu-cdaigle10 <msu-cdaigle10@mcneese.edu>
2025-04-11 14:57:07 -0500

    added companions and also made a sell function at shop

---
+++ Assets/scripts/CompanionScript/CompanionMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class CompanionMovement : MonoBehaviour
+{
+    public Animator playerAnimator; // drag the player’s Animator in the Inspector
+    private Animator foxAnimator;
+    public Transform playerTransform;
+    [SerializeField] public float followSpeed = 1f;
+    public Vector3 followOffset = new Vector3(-0.5f, -0.5f, 0);
+
+    void Start()
+    {
+        foxAnimator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        // Copy movement parameters
+        foxAnimator.SetFloat("MoveX", playerAnimator.GetFloat("MoveX"));
+        foxAnimator.SetFloat("MoveY", playerAnimator.GetFloat("MoveY"));
+        foxAnimator.SetFloat("LastMoveX", playerAnimator.GetFloat("LastMoveX"));
+        foxAnimator.SetFloat("LastMoveY", playerAnimator.GetFloat("LastMoveY"));
+        foxAnimator.SetFloat("MoveMagnitude", playerAnimator.GetFloat("MoveMagnitude"));
+
+        // Determine if player is moving (based on MoveMagnitude)
+        bool isMoving = playerAnimator.GetFloat("MoveMagnitude") > 0f;
+        foxAnimator.SetBool("IsMoving", isMoving);
+
+
+    }
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 1; // Limit to 60 FPS
+    }
+
+    void LateUpdate()
+    {
+        Vector3 targetPos = playerTransform.position + followOffset;
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, followSpeed * Time.deltaTime);
+    }
+}
