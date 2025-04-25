173b47325efcc9e36deb312aba49ec1199103db2
msu-cdaigle10 <msu-cdaigle10@mcneese.edu>
2025-04-25 18:57:14 -0500

    added exit prompt for tutorial

---
+++ Assets/scripts/Tutorial Scripts/TutorialExitPromptTrigger.cs
@@ -0,0 +1,72 @@
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class TutorialExitPromptTrigger : MonoBehaviour
+{
+    private bool playerInRange = false;
+    public GameObject prompt;
+    private bool isMapOpen = false;
+    private bool isInventoryOpen = false;
+
+    private void Start()
+    {
+        prompt.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            isMapOpen = !isMapOpen;
+
+            if (isMapOpen)
+            {
+                prompt.SetActive(false);
+            }
+            else if (playerInRange)
+            {
+                prompt.SetActive(true);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isInventoryOpen = !isInventoryOpen;
+            if (isInventoryOpen)
+            {
+                prompt.SetActive(false);
+            }
+            else if (playerInRange)
+            {
+                prompt.SetActive(true);
+            }
+        }
+
+        if (playerInRange && !isMapOpen && !isInventoryOpen && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("Hubworld");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            prompt.SetActive(true);
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            prompt.SetActive(false);
+            playerInRange = false;
+        }
+    }
+}
+
+
