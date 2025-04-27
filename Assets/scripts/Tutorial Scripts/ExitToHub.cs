b8faa0abfb47b42a23b2ad70134554eb84086fb3
Ryan Lebato <96466382+ryanlebato@users.noreply.github.com>
2025-04-26 20:05:01 -0500

    added tutorial player data scriptable object

--- Assets/scripts/Tutorial Scripts/ExitToHub.cs
+++ Assets/scripts/Tutorial Scripts/ExitToHub.cs
@@ -5,12 +5,22 @@ public class ExitToHub : MonoBehaviour
 {
     [SerializeField] private string sceneName = "Hubworld";
     private bool hasExited = false;
+    [SerializeField] private PlayerData playerData;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!hasExited && collision.CompareTag("Player"))
         {
             hasExited = true;
+            playerData.HP = 100;
+            playerData.Agility = 2;
+            playerData.Defense = 1;
+            playerData.Luck = 1;
+            playerData.MeleeDamage = 10;
+            playerData.RangedDamage = 10;
+            playerData.Experience = 0;
+            playerData.Level = 1;
+            playerData.AvailablePoints = 0;
             SceneManager.LoadScene(sceneName);
         }
     }
